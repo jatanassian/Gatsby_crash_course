d23fcc332e66e1fc971a52175d7fa3c8ba74ddd4
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-18 12:10:50 -0500

    import the Layout component to display the navbar

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const AboutPage = () => {
   return (
-    <div>
+    <Layout>
+      <SEO title="About us" />
       <h1>About Us</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae vero sint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil, amet corporis tempora odio explicabo soluta delectus.</p>
-    </div>
+    </Layout>
   )
 }
 
