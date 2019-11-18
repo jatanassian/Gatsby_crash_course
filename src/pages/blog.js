34661977e11f84e5734407a89377df579a14ac04
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-18 16:51:47 -0500

    iadd a link to the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,27 @@
-import React from "react"
+import React from "react";
+import { graphql } from 'gatsby';
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Link from 'gatsby-link';
 
-const BlogPage = () => (
+const BlogPage = ({data}) => (
   <Layout>
     <SEO title="Blog" />
-    <h1>Latest  Posts</h1>
+    <h1>Latest Posts</h1>
+    {data ? data.allMarkdownRemark.edges.map(post => {
+      return (
+      <div key={post.node.id}>
+        <h3>{post.node.frontmatter.title}</h3>
+        <small>Posted by {post.node.frontmatter.author} on {post.node.frontmatter.date}</small>
+        <br />
+        <br />
+        <Link to={post.node.frontmatter.path}>Read more</Link>
+        <br />
+        <br />
+        <hr />
+      </div>
+    )}) : null}
   </Layout>
 )
 
