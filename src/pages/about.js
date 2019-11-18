3baad6b86b560269c615d14a74e584a9be5ae63d
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-18 12:05:55 -0500

    add some random content to the page to test if it's displaying

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,8 @@ import React from 'react'
 const AboutPage = () => {
   return (
     <div>
-      
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae vero sint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil, amet corporis tempora odio explicabo soluta delectus.</p>
     </div>
   )
 }
