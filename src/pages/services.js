972198d618b388b77382e77a58ec46b32f7df1e0
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-18 12:11:15 -0500

    create the page with some lorem text to test if it's being displayed

---
+++ src/pages/services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const ServicesPage = () => {
+  return (
+    <Layout>
+      <SEO title="Our Services" />
+      <h1>Our Services</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae vero sint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil, amet corporis tempora odio explicabo soluta delectus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae vero sint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil, amet corporis tempora odio explicabo soluta delectus.</p>
+    </Layout>
+  )
+}
+
+export default ServicesPage;
