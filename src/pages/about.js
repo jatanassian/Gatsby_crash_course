c6b3a8998b36cfc5f4a1d83276f3bf6e5922d06a
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-18 13:59:14 -0500

    remove return and curly braces

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const AboutPage = () => {
-  return (
-    <Layout>
-      <SEO title="About us" />
-      <h1>About Us</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae vero sint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil, amet corporis tempora odio explicabo soluta delectus.</p>
-    </Layout>
-  )
-}
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About us" />
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae verosint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil,amet corporis tempora odio explicabo soluta delectus.</p>
+  </Layout>
+)
 
 export default AboutPage;
--- src/pages/services.js
+++ src/pages/services.js
@@ -2,15 +2,13 @@ import React from 'react';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const ServicesPage = () => {
-  return (
-    <Layout>
-      <SEO title="Our Services" />
-      <h1>Our Services</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae vero sint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil, amet corporis tempora odio explicabo soluta delectus.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae vero sint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil, amet corporis tempora odio explicabo soluta delectus.</p>
-    </Layout>
-  )
-}
+const ServicesPage = () => (
+  <Layout>
+    <SEO title="Our Services" />
+    <h1>Our Services</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae verosint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil,amet corporis tempora odio explicabo soluta delectus.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, ullam vitae. Harum odio blanditiis dolore molestiae verosint enim, necessitatibus molestias voluptatem, debitis suscipit facere unde quidem error laboriosam corrupti nihil ex. Nihil,amet corporis tempora odio explicabo soluta delectus.</p>
+  </Layout>
+)
 
 export default ServicesPage;
