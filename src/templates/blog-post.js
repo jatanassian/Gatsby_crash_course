5dedb71b8ba955c0298d4a94d56e6e8c84d81c29
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-18 17:15:46 -0500

    add layout and seo components to display the navbar

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import { graphql } from 'gatsby';
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 export default function Template({data}) {
-  const post = data.markedownRemark
+  const post = data.markdownRemark
 
   return (
-    <div>
+    <Layout>
+      <SEO title="Post" />
       <Link to='/blog'>Go Back</Link>
       <hr />
       <h1>{post.frontmatter.title}</h1>
       <h4>Posted by {post.frontmatter.author} on {post.frontmatter.date}</h4>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </div>
+    </Layout>
   )
 }
 
