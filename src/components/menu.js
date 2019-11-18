f96c11f2d9085b1ff6392dd2f302d68e1ed507bf
Julien Atanassian <julien.atanassian@gmail.com>
2019-11-18 14:03:36 -0500

    add inline style

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { NONAME } from 'dns';
+import Link from 'gatsby-link';
 
 const Menu = () => (
-  <div>
-    
+  <div style={{
+    background: '#f4f4f4',
+    paddingTop: '10px',
+  }}>
+    <ul style={{
+      listStyle: NONAME,
+      display: 'flex',
+      justifyContent: 'space-evenly'
+    }}>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 )
 
